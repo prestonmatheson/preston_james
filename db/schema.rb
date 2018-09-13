49922b47198e447c0ba1c88ccc69d4ff458ee6ac
Preston Matheson <pmphoto123@gmail.com>
2018-09-13 15:04:19 -0700

    added a subheading to blogs for the index page and added the button styles on the show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180826182211) do
+ActiveRecord::Schema.define(version: 20180913214034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180826182211) do
     t.string "subject"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "subheading"
   end
 
   create_table "portfolios", force: :cascade do |t|

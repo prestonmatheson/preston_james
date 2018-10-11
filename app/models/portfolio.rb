7ad410c964acfb6a5405dae369b41ae983ef6471
Preston Matheson <pmphoto123@gmail.com>
2018-10-11 11:50:28 -0700

    functional image uploading for portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,4 @@
 class Portfolio < ApplicationRecord
-  mount_uploader :main_image, :thumb_image, PhotoUploader
+  mount_uploader :main_image, PortfolioUploader
+  mount_uploader :thumb_image, PortfolioUploader
 end
--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,4 +1,4 @@
-class PhotoUploader < CarrierWave::Uploader::Base
+class PortfolioUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick

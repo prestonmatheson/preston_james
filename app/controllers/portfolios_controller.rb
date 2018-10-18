717f6df2fc910910fdfa6226cb543b387f04ee86
Preston Matheson <pmphoto123@gmail.com>
2018-10-17 21:42:57 -0700

    added position element to portfolio so we can order them for drag and drop

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,7 +5,7 @@ class PortfoliosController < ApplicationController
   # GET /portfolios
   # GET /portfolios.json
   def index
-    @portfolios = Portfolio.all
+    @portfolios = Portfolio.by_position
   end
 
   # GET /portfolios/1
